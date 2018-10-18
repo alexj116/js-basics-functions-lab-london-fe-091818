564a08d1bc0d465d99b76a2c5033cf52589a1e8e
Alexandra Johnson <alexandra@queryclick.com>
2018-10-18 19:56:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(block) {
   const hq=42;
-  if (block<hq) {
-    return hq-block;
+  if (block < hq) {
+    return hq - block;
   }
   else return distance - hq;
 
