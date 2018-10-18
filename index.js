8bc177d959f12dc4e98d94b5228c873ce42bbda8
Alexandra Johnson <alexandra@queryclick.com>
2018-10-18 19:28:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const hq=42
-function distanceFromHqInBlocks(hq,pickupLocation) {
+function distanceFromHqInBlocks(distance) {
   
 }
 function distanceFromHqInFeet(){
